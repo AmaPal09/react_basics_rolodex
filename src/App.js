569176f98e885503afc938428009cbc4cbbd3e26
Amarja Palekar <amarjapalekar09@gmail.com>
2022-12-13 16:31:06 -0800

    Remove all unnecessary code to start from scratch

--- src/App.js
+++ src/App.js
@@ -16,59 +16,10 @@ class App extends Component {
   render() {
     return (
       <div className="App"> 
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" /> 
-          <p> 
-            I am {this.state.name.firstName} {this.state.name.lastName}. I work with {this.state.company}. 
-          </p>
-          <button onClick={() => {
-            let obj1 = {pet: "dog"}; 
-            let obj2 = obj1; 
-            console.log(obj1 === obj2); 
-            obj1.pet = "cat"; 
-            console.log(obj1 === obj2); 
-            let obj3 = Object.assign({}, obj1); 
-            console.log(obj1 === obj3); 
-            console.log("obj1: ", obj1); 
-            console.log("obj2: ", obj2); 
-
-            this.setState(() => {
-              return {
-                name: {firstName: 'Amarja', lastName: 'Palekar'}
-              };
-            }, () => {
-              console.log(this.state);
-            }); 
-            // console.log(this.state);
-            } }>
-            Change name
-          </button>
-        </header>
       </div>
     ); 
   }
 } 
-/* 
-// function App() {
-//   return (
-//     <div className="App">
-//       <header className="App-header">
-//         <img src={logo} className="App-logo" alt="logo" />
-//         <p>
-//           Hello my name is Amarja! And this is my react app
-//         </p>
-//         <a
-//           className="App-link"
-//           href="https://reactjs.org"
-//           target="_blank"
-//           rel="noopener noreferrer"
-//         >
-//           Learn React
-//         </a>
-//       </header>
-//     </div>
-//   );
-// }
-*/
+
 
 export default App;
