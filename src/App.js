4aca770d31ba930a9b8d28c103c6710d96cbe61c
Amarja Palekar <amarjapalekar09@gmail.com>
2022-12-16 13:15:14 -0800

    Add console statements to each method to track execution flow

--- src/App.js
+++ src/App.js
@@ -10,9 +10,12 @@ class App extends Component {
     this.state = {
       monsters : [ ],
     }
+
+    console.log("constructor");
   }
 
   componentDidMount() {
+    console.log("componentDidMount");
     fetch('https://jsonplaceholder.typicode.com/users')
       .then(response => response.json())
       .then((users) => this.setState( () => {
@@ -22,6 +25,7 @@ class App extends Component {
   } 
 
   render() {
+    console.log("render");
     return (
       <div className="App"> 
         {this.state.monsters.map((monster) => {
