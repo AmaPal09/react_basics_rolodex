26e150a1ce3b62b9511f05a95ebf85beeed06be8
Amarja Palekar <amarjapalekar09@gmail.com>
2022-12-19 15:21:39 -0800

    Use individual component to render list of monster names

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import { Component } from 'react'; 
 
-import logo from './logo.svg';
+import CardList from './components/card-list/card-list.component.jsx'; 
 import './App.css';
 
 class App extends Component {
@@ -51,10 +51,11 @@ class App extends Component {
           placeholder="search monsters" 
           onChange={onSearchChange}/>
 
-        {filteredMonsters.map((monster) => {
+       {/* {filteredMonsters.map((monster) => {
             return <h1 key={monster.id}>{monster.name}</h1>; 
           })
-        } 
+        }  */}
+        <CardList /> 
       </div>
     ); 
   }
---
+++ src/components/card-list/card-list.component.jsx
@@ -0,0 +1,13 @@
+import { Component } from 'react';
+
+class CardList extends Component {
+    render(){
+        return (
+            <div>
+                Hello I am a card list. 
+            </div>
+        );
+    }
+}
+
+export default CardList; 
