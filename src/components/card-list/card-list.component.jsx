b288c63e3a8e4c3cbf1a3856ad0ba083d0f7de0b
Amarja Palekar <amarjapalekar09@gmail.com>
2022-12-19 18:36:23 -0800

    Use extrapolated values

--- src/components/card-list/card-list.component.jsx
+++ src/components/card-list/card-list.component.jsx
@@ -12,12 +12,12 @@ class CardList extends Component {
                 { monsters.map(monster => {
                     const {id, name, email} = monster;  
                     return ( 
-                        <div className='card-container' key='monster.id'>
-                            <img alt= {`monster ${monster.name}`} 
+                        <div className='card-container' key='id'>
+                            <img alt= {`monster ${name}`} 
                             className='img' 
-                            src={`https://robohash.org/${monster.id}?set=set2size=180x180`} />
-                            <h2>{monster.name}</h2>
-                            <p>{monster.email}</p>
+                            src={`https://robohash.org/${id}?set=set2size=180x180`} />
+                            <h2>{name}</h2>
+                            <p>{email}</p>
                         </div>
                     // <h1 key={monster.id}>{monster.name}</h1>
                     )
