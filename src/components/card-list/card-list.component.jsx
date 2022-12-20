dc4728aea5179f1c45b53304b3a8bc481fdf6d09
Amarja Palekar <amarjapalekar09@gmail.com>
2022-12-19 18:23:34 -0800

    Style components

--- src/components/card-list/card-list.component.jsx
+++ src/components/card-list/card-list.component.jsx
@@ -1,14 +1,23 @@
 import { Component } from 'react';
 
+import './card-list.styles.css'; 
+
 class CardList extends Component {
     
     render(){
         // console.log(this.props);    
         const {monsters} = this.props; 
         return (
-            <div>
+            <div className='card-list'>
                 { monsters.map(monster => {
-                    return <h1 key={monster.id}>{monster.name}</h1>
+                    return <div className='card-container' key='monster.id'>
+                        <img alt= {`monster ${monster.name}`} 
+                        className='img' 
+                        src={`https://robohash.org/${monster.id}?set=set2size=180x180`} />
+                        <h2>{monster.name}</h2>
+                        <p>{monster.email}</p>
+                    </div>
+                    // <h1 key={monster.id}>{monster.name}</h1>
                 })}
             </div>
         );
