82b05cb1ffc5c3041b8f72ac2bdedd6717e751bc
Amarja Palekar <amarjapalekar09@gmail.com>
2023-04-19 13:46:33 -0700

    MonsterRolodex - Remaining Components
    Change card container component

--- src/components/card-container/card-container.component.jsx
+++ src/components/card-container/card-container.component.jsx
@@ -1,22 +1,17 @@
-import { Component } from 'react'; 
-
 import './card-container.styles.css';
 
-class CardContainer extends Component {
-    render() {
-        const {monster} = this.props; 
-        const { id, name, email} = monster; 
-        // console.log(monster);
-        return (
-            <div className='card-container' key ='id'>
-                <img alt= {`monster ${name}`} 
-                    className='img' 
-                    src={`https://robohash.org/${id}?set=set2size=180x180`} />
-                <h2>{name}</h2>
-                <p>{email}</p>
-            </div>
-        ); 
-    }
+const CardContainer = ( { monster } ) =>  {
+    const { id, name, email} = monster; 
+    // console.log(monster);
+    return (
+        <div className='card-container' key ='id'>
+            <img alt= {`monster ${name}`} 
+                className='img' 
+                src={`https://robohash.org/${id}?set=set2size=180x180`} />
+            <h2>{name}</h2>
+            <p>{email}</p>
+        </div>
+    ); 
 }
 
 export default CardContainer; 
