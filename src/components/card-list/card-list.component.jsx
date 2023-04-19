7b6623d8a9028c172ca573ba98f6c6435da2cbb9
Amarja Palekar <amarjapalekar09@gmail.com>
2023-04-19 13:44:12 -0700

    MonstoreRolodex - Remaining Components
    Change card list component

--- src/components/card-list/card-list.component.jsx
+++ src/components/card-list/card-list.component.jsx
@@ -1,22 +1,13 @@
-import { Component } from 'react';
-
-import './card-list.styles.css'; 
+import './card-list.styles.css';
 import CardContainer from '../card-container/card-container.component';
 
-class CardList extends Component {
-    
-    render(){
-        // console.log(this.props);    
-        const {monsters} = this.props; 
-        return (
-            <div className='card-list'>
-                { monsters.map(monster => {
-                    // console.log(monster); 
-                    return <CardContainer monster = { monster }/> 
-                })}
-            </div>
-        );
-    }
-}
+const CardList = ({ monsters }) =>  (
+    <div className='card-list'>
+        { monsters.map(monster => {
+            // console.log(monster);
+            return <CardContainer monster = { monster }/>
+        })}
+    </div>
+);
 
-export default CardList; 
+export default CardList;
