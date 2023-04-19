8ce4465789a62d0ed2a453b1311b2e9784fe6d09
Amarja Palekar <amarjapalekar09@gmail.com>
2023-04-19 12:55:13 -0700

    Monster Rolodex: Infinite Re-rendering

--- src/App.js
+++ src/App.js
@@ -1,40 +1,50 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 
-// import CardList from './components/card-list/card-list.component.jsx'; 
+import CardList from './components/card-list/card-list.component.jsx';
 import SearchBox from './components/search-box/search-box.component.jsx';
 import './App.css';
 
 const App = () => {
 
     const [searchField, setSearchField] = useState(''); //[value, setValue_function]
-    console.log(searchField);  
-    
+    const [monsters, setMonsters] = useState([]);//[value, setValue_function]
+    console.log('render');
+
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then(response => response.json())
+      .then((users) => setMonsters(users));
+
+
     const onSearchChange = (event) => {
-        const searchFielString = event.target.value.toLocaleLowerCase(); 
-        setSearchField(searchFielString); 
+        const searchFieldString = event.target.value.toLocaleLowerCase();
+        setSearchField(searchFieldString);
     }
+
+    const filteredMonsters = monsters.filter((monster) => {
+      return monster.name.toLocaleLowerCase().includes(searchField);
+    });
 
     return (
         <div className="App">
           <h1 className="app-title">Monsters Rolodex</h1>
-          <SearchBox 
-            className="monsters-search-box" 
+          <SearchBox
+            className="monsters-search-box"
             placeholder="search-monster"
             onChangeHandler={onSearchChange}
-          /> 
-  
-          {/* <CardList monsters = {filteredMonsters} />  */}
+          />
+
+          <CardList monsters = {filteredMonsters} />
         </div>
-      ); 
+      );
 }
 
 // class App extends Component {
 //   constructor() {
-//     super(); 
+//     super();
 
 //     this.state = {
 //       monsters : [ ],
-//       searchField : '', 
+//       searchField : '',
 //     }
 
 //     // console.log("constructor");
@@ -46,43 +56,43 @@ const App = () => {
 //       .then(response => response.json())
 //       .then((users) => this.setState( () => {
 //         return {monsters: users}
-//       }, 
+//       },
 //       // () => {console.log(this.state)}
-//       )); 
-//   } 
+//       ));
+//   }
 
 //   onSearchChange = (event) => {
 //     console.log(event.target.value);
-//     const searchField = event.target.value.toLocaleLowerCase(); 
+//     const searchField = event.target.value.toLocaleLowerCase();
 //     this.setState(() => {
-//       return {searchField}; 
-//     }); 
+//       return {searchField};
+//     });
 // }
 
 //   render() {
 //     const {monsters, searchField} = this.state;
-//     const { onSearchChange } = this; 
+//     const { onSearchChange } = this;
 
 //     // console.log("render");
 //     const filteredMonsters = monsters.filter((monster) => {
-//       return monster.name.toLocaleLowerCase().includes(searchField); 
-//     }); 
+//       return monster.name.toLocaleLowerCase().includes(searchField);
+//     });
 
 
 //     return (
 //       <div className="App">
 //         <h1 className="app-title">Monsters Rolodex</h1>
-//         <SearchBox 
-//           className="monsters-search-box" 
+//         <SearchBox
+//           className="monsters-search-box"
 //           placeholder="search-monster"
 //           onChangeHandler={onSearchChange}
-//         /> 
+//         />
 
-//         <CardList monsters = {filteredMonsters} /> 
+//         <CardList monsters = {filteredMonsters} />
 //       </div>
-//     ); 
+//     );
 //   }
-// } 
+// }
 
 
 export default App;
